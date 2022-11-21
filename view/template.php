6cc8ec64c22f1538142bf4170230c76ca3755e92
Tebiog <tekityt@gmail.com>
2022-11-21 10:10:12 +0200

    fix

--- view/template.php
+++ view/template.php
@@ -72,5 +72,6 @@ $data['options']['lang'] ?>">
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script src="/assets/js/scrollRevAnimation.js"></script>
+
 </body>
 </html>
