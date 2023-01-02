b8aed64d1f8599b38755b0070755e32344f88c61
Tebiog <tekityt@gmail.com>
2023-01-02 14:12:11 +0200

    fix

--- assets/js/User/Shop/cartScript.js
+++ assets/js/User/Shop/cartScript.js
@@ -22,47 +22,49 @@ window.addEventListener("load", function () {
         $elemContainer.append($buyBtnContainer);
 
         if (hasPressed === false) {
+            if(cartArr.length > 0){
+                let summaryPrise = 0;
+                $cartContainer.fadeIn(500);
+                hasPressed = true;
 
-            let summaryPrise = 0;
-            $cartContainer.fadeIn(500);
-            hasPressed = true;
-
-            for (let i = 0; i < cartArr.length; i++) {
-
-                let cartItem = $(`<li class="row cart-li">
-                    <p>${cartArr[i].name}</p>
-                    <p>${cartArr[i].price}</p>
+                for (let i = 0; i < cartArr.length; i++) {
+                    let cartItem = $(`<li class="cart-li">
+                    <div class="cart-product-name">${cartArr[i].name}
+                        <span class="tooltiptext">${cartArr[i].name}</span>
+                    </div>
+                    <p class="cart-product-price">${cartArr[i].price}</p>
                     <a type="button" class="remove-cart-elem">del</a>
                 </li>`);
 
-                cartItem.find(".remove-cart-elem").on("click", () => {
-                    summaryPrise -= parseInt(cartArr[i].price);
-                    cartArr[i] = null;
-                    cartItem.remove();
-                    if(summaryPrise <= 0){
-                        $buyBtnContainer.remove();
-                    }
+                    cartItem.find(".remove-cart-elem").on("click", () => {
+                        summaryPrise -= parseInt(cartArr[i].price);
+                        cartArr[i] = null;
+                        cartItem.remove();
+                        if(summaryPrise <= 0){
+                            $buyBtnContainer.remove();
+                        }
+                        $(".summary-prise").text("summary price: "+summaryPrise+"$");
+                    })
+
+                    $elemContainer.append(cartItem);
+                    summaryPrise += parseInt(cartArr[i].price);
+
+                }
+                if (summaryPrise > 0) {
+                    paypal.Buttons({
+                        createOrder: function(data, actions) {
+                            // Set up the transaction
+                            return actions.order.create({
+                                purchase_units: [{
+                                    amount: {
+                                        value: summaryPrise
+                                    }
+                                }]
+                            });
+                        }
+                    }).render('#paypal-button-container');
                     $(".summary-prise").text("summary price: "+summaryPrise+"$");
-                })
-
-                $elemContainer.append(cartItem);
-                summaryPrise += parseInt(cartArr[i].price);
-
-            }
-            if (summaryPrise > 0) {
-                paypal.Buttons({
-                    createOrder: function(data, actions) {
-                        // Set up the transaction
-                        return actions.order.create({
-                            purchase_units: [{
-                                amount: {
-                                    value: summaryPrise
-                                }
-                            }]
-                        });
-                    }
-                }).render('#paypal-button-container');
-                $(".summary-prise").text("summary price: "+summaryPrise+"$");
+                }
             }
         } else {
             $cartContainer.fadeOut(500);
