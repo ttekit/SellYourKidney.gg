884ce2ae2ada561f6397f8e953a304e629d0ff13
Tebiog <tekityt@gmail.com>
2023-01-06 14:04:58 +0200

    cart fix

--- assets/js/User/Shop/cartScript.js
+++ assets/js/User/Shop/cartScript.js
@@ -43,13 +43,14 @@ window.addEventListener("load", function () {
                 hasPressed = true;
 
                 for (let i = 0; i < cartArr.length; i++) {
-                    let cartItem = $(`
+                    if(cartArr != null){
+                        let cartItem = $(`
                 <li class="cart-li">
                     <div class="count-cart-elem">${cartArr[i].prodCount}</div>
                     <div class="cart-product-name">${cartArr[i].name}
                         <span class="tooltiptext">${cartArr[i].name}</span>
                     </div>
-                    <p class="cart-product-price">${cartArr[i].price}</p>
+                    <p class="cart-product-price">${parseInt(cartArr[i].price) * cartArr[i].prodCount} $</p>
                     <div class="count-cart-elems">
                         <div class="count-cart-buttons">
                             <a type="button" class="add-cart-elem"></a>
@@ -60,42 +61,43 @@ window.addEventListener("load", function () {
                     <a type="button" class="remove-cart-elem">x</a>
                 </li>`);
 
-                    cartItem.find(".remove-cart-elem").on("click", () => {
-                        summaryPrise -= parseInt(cartArr[i].price) * parseInt(cartArr[i].prodCount);
-                        cartArr[i] = null;
-                        cartItem.remove();
-                        if (summaryPrise <= 0) {
-                            cartArr = [];
-                            $cartContainer.fadeOut(500);
-                            hasPressed = false;
-                            $elemContainer.empty();
-                        }
-                        $(".summary-prise").text("summary price: " + summaryPrise + "$");
-                    })
+                        cartItem.find(".remove-cart-elem").on("click", () => {
+                            summaryPrise -= parseInt(cartArr[i].price) * parseInt(cartArr[i].prodCount);
+                            cartArr[i] = null;
+                            cartItem.remove();
+                            if (summaryPrise <= 0) {
+                                cartArr = [];
+                                $cartContainer.fadeOut(500);
+                                hasPressed = false;
+                                $elemContainer.empty();
+                            }
+                            $(".summary-prise").text("summary price: " + summaryPrise + "$");
+                        })
 
-                    cartItem.find(".add-cart-elem").on("click", (e) => {
-                        summaryPrise += parseInt(cartArr[i].price);
-                        cartArr[i].prodCount++;
-                        $(e.target).closest(".cart-product-price").text(cartArr[i].prodCount);
-                        $(".summary-prise").text("summary price: " + summaryPrise + "$");
+                        cartItem.find(".add-cart-elem").on("click", (e) => {
+                            summaryPrise += parseInt(cartArr[i].price);
+                            cartArr[i].prodCount++;
 
-                        $(e.target).closest("li").find(".cart-product-price").text(parseInt(cartArr[i].price) * cartArr[i].prodCount + "$");
-                    });
-
-                    cartItem.find(".decrease-cart-elem").on("click", (e) => {
-                        if (cartArr[i].prodCount > 1) {
-                            summaryPrise -= parseInt(cartArr[i].price);
-                            cartArr[i].prodCount--;
-                            cartItem.find(".count-cart-elem").text(cartArr[i].prodCount);
-
+                            $(e.target).closest("li").find(".count-cart-elem").text(cartArr[i].prodCount);
+                            $(".summary-prise").text("summary price: " + summaryPrise + "$");
                             $(e.target).closest("li").find(".cart-product-price").text(parseInt(cartArr[i].price) * cartArr[i].prodCount + "$");
-                        }
+                        });
 
-                    })
+                        cartItem.find(".decrease-cart-elem").on("click", (e) => {
+                            if (cartArr[i].prodCount > 1) {
+                                summaryPrise -= parseInt(cartArr[i].price);
+                                cartArr[i].prodCount--;
+
+                                cartItem.find(".count-cart-elem").text(cartArr[i].prodCount);
+                                $(e.target).closest("li").find(".cart-product-price").text(parseInt(cartArr[i].price) * cartArr[i].prodCount + "$");
+                            }
+
+                        })
 
 
-                    $elemContainer.append(cartItem);
-                    summaryPrise += parseInt(cartArr[i].price);
+                        $elemContainer.append(cartItem);
+                        summaryPrise += parseInt(cartArr[i].price);
+                    }
 
                 }
                 if (summaryPrise > 0) {
